ddf330c6881d4b44241a613685b4a967e608b264
Kyu shi <130895055+kyushiartz@users.noreply.github.com>
2024-04-09 15:44:38 +0800

    Add files via upload

--- js/contact-validation.js
+++ js/contact-validation.js
@@ -6,7 +6,7 @@ var usernamevalid = document.getElementById("username-valid");
 // promts a message info to the user if the contact form is valid or not
 function validateUsername(){
   if(!usernamefield.value.match(/^[A-Za-z]*[\ \][a-z]{3,25}$/)){
-    usernameerror.innerHTML ="Must be at least 1-3 characters no number/special char!";
+    usernameerror.innerHTML ="Must be atleast 1-3 characters no number/special char!";
     usernamefield.style.borderColor ="red";
     usernamevalid.innerHTML ="";
     usernamelabel.style.color ="red";
