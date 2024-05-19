e7d416f3b741fa77fb793b105e955b97127cdf5c
Kyu shi <130895055+kyushiartz@users.noreply.github.com>
2024-05-19 23:18:45 +0800

    Update

--- js/filter.js
+++ js/filter.js
@@ -2,493 +2,493 @@ document.addEventListener("DOMContentLoaded", () => {
   const images = [
     {
       src: "images/artgallary/Awakened_from_a_deep_slumber (2024).jpg",
-      title: "Elishia_chibi_grayscale (2023)",
-      category: "grayscale"
+      title: "",
+      category: "semi-realistic"
     },
     {
       src: "images/artgallary/Elishia_portrait (2023).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
       src: "images/artgallary/Liliana_portrait (2023).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
       src: "images/artgallary/Tekushiki (2023).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
       src: "images/artgallary/Feya_landscape (2023).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
       src: "images/artgallary/Feya_portrait (2023).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
       src: "images/artgallary/Elishia_chibi_grayscale (2023).jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Tragic_past_grayscale (2023).jpg",
       title: "",
       category: "grayscale"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Former_falcon_squad (2023).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Old_rivals_grayscale (2023).jpg",
       title: "",
       category: "grayscale"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Until_we_meet_again (2022).jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Elishia_portrait (2022).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Fall_into_despair (2022).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Wednesday_Fanart (2022).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Rebecca_portrait_fanart (2022).jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/A_mothers_love_grayscale (2022).jpg",
       title: "",
       category: "grayscale"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Fallen_angel (2022).jpg",
       title: "",
       category: "grayscale"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Raging_blood_of_the_past (2022).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/The_return (2022).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Silent_killer (2022).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Hidden_truth (2022).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Mission_one (2022).jpg",
       title: "",
       category: "grayscale"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/R9 (2022).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Elishia_Summer_grayscale (2022).jpg",
       title: "",
       category: "grayscale"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Bday_art (2022).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Elishia_padoru (2022).jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Teku_padoru (2022).jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Restricted_zone (2022).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Elishia_younger_self (2022).jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Childhood_bestfriend (2022).jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/RB (2022).jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/The_twin_dragon_blade (2022).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Ren_Rayza (2022).jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Hexxana_bust_up_graycale (2022).jpg",
       title: "",
       category: "grayscale"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Hexx_emote.jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/dark_legion_elite_grayscale (2022).jpg",
       title: "",
       category: "grayscale"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Inner_demon (2022).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Hexxana_star_dust_witch (2022).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Enemy_territory (2021).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Elishia_cute_emote.jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Hexxana_cute_emote.jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Ren_cute_emote.jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Rayza_cute_emote.jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Drakion_cute_emote.jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Arzhel_cute_emote.jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Stelia_cute_emote.jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Shelain_cute_emote.jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Flora_cute_emote.jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/H.R.E. (2021).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Day_off (2021).jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Christmas_theme (2020).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/RB9_cover (2020).jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Elishia_portrait (2020).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Hexxana_portrait (2020).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Rayza_portrait (2020).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Stellia_portrait (2020).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Blessica_portrait (2020).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Journey (2020).jpg",
       title: "",
-      category: "grayscale"
+      category: "all"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Ruins (2020).jpg",
       title: "",
-      category: "grayscale"
+      category: "all"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Elishia_Hexxana (2020).jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Tekushiki_portrait_fanart (2020).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Duel (2020).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Hexxana_tekushiki (2020).jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Shelain (2020).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Hexxana_full_body (2020).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Rayza_full_body (2020).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Hexxana_star_dust_witch (2020).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/First_characters (2020).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Stelia_full_body (2020).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/humanoid (2019).jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Hexxana_star_dust_witch (2019).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Hexxana_stardust_witch (2019).jpg",
       title: "",
-      category: "grayscale"
+      category: "semi-realistic"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Fan_art_gate (2019).jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Hexxana_first_digital_portrait.jpg",
       title: "",
-      category: "grayscale"
+      category: "anime-style"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/Untitled5_20210915105136.jpg",
       title: "",
-      category: "grayscale"
+      category: "traditionl"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/IMG_20190531_235310.jpg",
       title: "",
-      category: "grayscale"
+      category: "traditionl"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/IMG_20190601_000810.jpg",
       title: "",
-      category: "grayscale"
+      category: "traditionl"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/IMG_20190601_000009.jpg",
       title: "",
-      category: "grayscale"
+      category: "traditionl"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/IMG_20190601_000538.jpg",
       title: "",
-      category: "grayscale"
+      category: "traditionl"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/IMG_20190601_001455.jpg",
       title: "",
-      category: "grayscale"
+      category: "traditionl"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/IMG_20190601_001711.jpg",
       title: "",
-      category: "grayscale"
+      category: "traditionl"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/IMG_20190601_002146.jpg",
       title: "",
-      category: "grayscale"
+      category: "traditionl"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/IMG_20190601_002447.jpg",
       title: "",
-      category: "grayscale"
+      category: "traditionl"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/IMG_20190601_004555.jpg",
       title: "",
-      category: "grayscale"
+      category: "traditionl"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/IMG_20190601_011919.jpg",
       title: "",
-      category: "grayscale"
+      category: "traditionl"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/IMG_20190601_012413.jpg",
       title: "",
-      category: "grayscale"
+      category: "traditionl"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/IMG_20190601_013045.jpg",
       title: "",
-      category: "grayscale"
+      category: "traditionl"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/IMG_20190616_230159.jpg",
       title: "",
-      category: "grayscale"
+      category: "traditionl"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/IMG_20190616_230228.jpg",
       title: "",
-      category: "grayscale"
+      category: "traditionl"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/IMG_20190903_223841.jpg",
       title: "",
-      category: "grayscale"
+      category: "traditionl"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/IMG_20191029_213136.jpg",
       title: "",
-      category: "grayscale"
+      category: "traditionl"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/IMG_20191222_180715.jpg",
       title: "",
-      category: "grayscale"
+      category: "traditionl"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/IMG_20200610_170001.jpg",
       title: "",
-      category: "grayscale"
+      category: "traditionl"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/IMG_20200612_113520.jpg",
       title: "",
-      category: "grayscale"
+      category: "traditionl"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/IMG_20200716_112918.jpg",
       title: "",
-      category: "grayscale"
+      category: "traditionl"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/IMG20200609154915.jpg",
       title: "",
-      category: "grayscale"
+      category: "traditionl"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/IMG20190827203601.jpg",
       title: "",
-      category: "grayscale"
+      category: "traditionl"
     },
     {
-      src: "images/artgallary/",
+      src: "images/artgallary/IMG20200410130753.jpg",
       title: "",
-      category: "grayscale"
+      category: "traditionl"
     },
     
     // Add more images here
