264139d5675b232fdd84febe9993b77d83b40982
Kyu shi <130895055+kyushiartz@users.noreply.github.com>
2024-05-23 23:09:07 +0800

    Update

--- js/filter.js
+++ js/filter.js
@@ -8,32 +8,32 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       src: "images/artgallary/Awakened_from_a_deep_slumber (2024).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Elishia_portrait (2023).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Liliana_portrait (2023).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Tekushiki (2023).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Feya_landscape (2023).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Feya_portrait (2023).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Elishia_chibi_grayscale (2023).jpg",
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       src: "images/artgallary/Former_falcon_squad (2023).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Old_rivals_grayscale (2023).jpg",
@@ -63,17 +63,17 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       src: "images/artgallary/Elishia_portrait (2022).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Fall_into_despair (2022).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Wednesday_Fanart (2022).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Rebecca_portrait_fanart (2022).jpg",
@@ -93,22 +93,22 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       src: "images/artgallary/Raging_blood_of_the_past (2022).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/The_return (2022).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Silent_killer (2022).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Hidden_truth (2022).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Mission_one (2022).jpg",
@@ -118,7 +118,7 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       src: "images/artgallary/R9 (2022).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Elishia_Summer_grayscale (2022).jpg",
@@ -128,7 +128,7 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       src: "images/artgallary/Bday_art (2022).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Elishia_padoru (2022).jpg",
@@ -143,7 +143,7 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       src: "images/artgallary/Restricted_zone (2022).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Elishia_younger_self (2022).jpg",
@@ -163,7 +163,7 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       src: "images/artgallary/The_twin_dragon_blade (2022).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Ren_Rayza (2022).jpg",
@@ -188,17 +188,17 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       src: "images/artgallary/Inner_demon (2022).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Hexxana_star_dust_witch (2022).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Enemy_territory (2021).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Elishia_cute_emote.jpg",
@@ -248,7 +248,7 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       src: "images/artgallary/H.R.E. (2021).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Day_off (2021).jpg",
@@ -258,7 +258,7 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       src: "images/artgallary/Christmas_theme (2020).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/RB9_cover (2020).jpg",
@@ -268,27 +268,27 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       src: "images/artgallary/Elishia_portrait (2020).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Hexxana_portrait (2020).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Rayza_portrait (2020).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Stellia_portrait (2020).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Blessica_portrait (2020).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Journey (2020).jpg",
@@ -308,12 +308,12 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       src: "images/artgallary/Tekushiki_portrait_fanart (2020).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Duel (2020).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Hexxana_tekushiki (2020).jpg",
@@ -323,32 +323,32 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       src: "images/artgallary/Shelain (2020).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Hexxana_full_body (2020).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Rayza_full_body (2020).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Hexxana_star_dust_witch (2020).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/First_characters (2020).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Stelia_full_body (2020).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/humanoid (2019).jpg",
@@ -358,12 +358,12 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       src: "images/artgallary/Hexxana_star_dust_witch (2019).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Hexxana_stardust_witch (2019).jpg",
       title: "",
-      category: "semi-realistic"
+      category: "semi-realism"
     },
     {
       src: "images/artgallary/Fan_art_gate (2019).jpg",
