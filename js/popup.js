9bd57def1e359af326845a21de55c5d6d91ed8da
Kyu shi <kyuushi992@gmail.com>
2024-09-17 13:09:07 +0800

    Update

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,15 @@
 let popup = document.getElementById("popup");
+let donatePopup = document.getElementById("donate-popup");
   
 function openPopup(){
   popup.classList.add("open-popup");
 }
 function closePopup(){
   popup.classList.remove("open-popup");
+}
+function openDonate(){
+  donatePopup.classList.add("open-donate");
+}
+function closeDonate(){
+  donatePopup.classList.remove("open-donate");
 }
